a7ad5ed49c8bbdde4ef15526d5a53011b3665447
goswamikaushik <goswamikaushik2003@gmail.com>
2024-04-09 12:13:33 +0530

    configure store provider and wrapp on whole application

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { store } from "./store";
 
 const el = document.getElementById("root");
-
 const root = createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
